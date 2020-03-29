0cda2dfd1c5fd732749cf1449edb9d172d623b88
Paweł Wołkowski <pawel.wolkowski@ialbatros.com>
2020-03-29 20:18:48 +0200

    add swagger

--- KWMonitor/KWMonitor/Startup.cs
+++ KWMonitor/KWMonitor/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace KWMonitor
 {
@@ -21,9 +22,14 @@ namespace KWMonitor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
             services.AddDbContext<KWMContext>
             (options =>
                 options.UseSqlite("Data Source=c:\\temp\\KWMonitor.db"));
+            services.AddTransient<KWMContext, KWMContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +41,11 @@ namespace KWMonitor
             }
 
             app.UseHttpsRedirection();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
